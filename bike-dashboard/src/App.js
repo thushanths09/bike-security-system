e1e6227b73b453375bebe5379989ddcc2ee348bb
Thushanth Sivashanthan <thushanths09@gmail.com>
2025-09-14 03:42:01 +0530

    build

--- bike-dashboard/src/App.js
+++ bike-dashboard/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import BikeMap from "./components/BikeMap";
 import AlertsList from "./components/AlertsList";
 import "./App.css";
