fb2275e75017a23bebbc967e5c0ec03f5634e161
Thushanth Sivashanthan <thushanths09@gmail.com>
2025-09-14 03:44:35 +0530

    change

--- bike-dashboard/src/index.js
+++ bike-dashboard/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; // ← import BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* ← wrap App with BrowserRouter */}
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
